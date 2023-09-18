26809eb94f7afb6fbab68ddfcdcd75ef507477de
acgoularte <augusto.silva@outlook.com>
2023-09-18 16:35:53 -0300

    update desafio_logica_DIO.js

--- Desafio_Logica_DIO.js
+++ Desafio_Logica_DIO.js
@@ -1,2 +1,50 @@
-let pokemon = ["POOCHYENA", "ZIGZAGOON", "DRAGONITE", "ORAGONITE", "POOCHYENA", "WURMPLE"]
+let heroi = "NCSK";
+let xp = 1001;
+let nivel = "";
+let i = 0;
 
+while (i < 12000) {
+  if (xp <= 1000) {
+    nivel = "Ferro";
+    //console.log('O Herói', heroi, 'está no nivel', nivel); // linha incluida para testar as condições
+    i = i + 500;
+    //xp = xp + 500 // linha incluida para testar as condições
+  } else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+    //console.log('O Herói', heroi, 'está no nivel', nivel); // linha incluida para testar as condições
+    i = i + 500;
+    //xp = xp + 500 // linha incluida para testar as condições
+  } else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+    //console.log('O Herói', heroi, 'está no nivel', nivel); // linha incluida para testar as condições
+    i = i + 500;
+    //xp = xp + 500 // linha incluida para testar as condições
+  } else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+    //console.log('O Herói', heroi, 'está no nivel', nivel); // linha incluida para testar as condições
+    i = i + 500;
+    //xp = xp + 500 // linha incluida para testar as condições
+  } else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+    //console.log('O Herói', heroi, 'está no nivel', nivel); // linha incluida para testar as condições
+    i = i + 500;
+    //xp = xp + 500 // linha incluida para testar as condições
+  } else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+    //console.log('O Herói', heroi, 'está no nivel', nivel); // linha incluida para testar as condições
+    i = i + 500;
+    //xp = xp + 500 // linha incluida para testar as condições
+  } else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+    //console.log('O Herói', heroi, 'está no nivel', nivel); // linha incluida para testar as condições
+    i = i + 500;
+    //xp = xp + 500 // linha incluida para testar as condições
+  } else if (xp >= 10001) {
+    nivel = "Radiante";
+    //console.log('O Herói', heroi, 'está no nivel', nivel); // linha incluida para testar as condições
+    i = i + 500;
+    //xp = xp + 500 // linha incluida para testar as condições
+  }
+}
+
+console.log("O Herói", heroi, "está no nivel", nivel);
